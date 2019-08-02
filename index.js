f5c01be3c86a19af53db78e346a3fc14b621d5c9
Evan  Markert <evanmarkert@gmail.com>
2019-08-02 20:31:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+const hq = 42;
+
+function distanceFromHqInBlocks(distance) {
+  return Math.abs(Number.parseInt(distance) - ${hq})
+};
+
+function distanceFromHqInFeet() {
+  
+};
+
+function distanceTravelledInFeet(){
+  
+};
+
+function calculatesFarePrice(start, destination) {
+  
+};
+
